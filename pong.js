e2610d73722123d0eb4110b68c449e93d15c4e65
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-06-23 18:57:26 -0400

    Clean up paddle code. Center the paddle.
    You can also choose which side the paddle starts on.

--- pong.js
+++ pong.js
@@ -1,49 +1,52 @@
 var canvas = document.getElementById("pong_canvas");
 var context = canvas.getContext("2d");
-var i = 0;
 var mousePos = {x: 0, y: 0}
 
-function paddle(x_pos, y_pos, is_human) {
-    this.x_pos = x_pos;
-    this.y_pos = y_pos;
+function paddle(is_left, y_pos, is_human) {
     this.width = 25;
     this.height = 100;
+
+    // Coordinates refer to the upper left corner of the paddle.
+    // IDEA: Change to exact center of paddle.
+    if(is_left) {
+        this.x_pos = 20;
+    } else {
+        this.x_pos = canvas.width - (this.width + 20)
+    }
+    this.y_pos = y_pos;
     this.is_human = is_human;
 
     // Maximum allowed distance to the edges.
-    this.min_y = 10;
-    this.max_y = canvas.height - (10 + this.height);
-    this.y_border = 50;
+    this.top_border = 10;
+    this.bottom_border = canvas.height - (this.top_border + this.height);
+    this.left_border = 20;
+    this.right_border = 20;
 
     this.draw = function() {
         context.fillStyle = "white";
         if (is_human) {
-            this.y_pos = mousePos.y
+            this.y_pos = mousePos.y - this.height/2 // Center paddle.
         }
-        if (this.y_pos > this.max_y) {
-            this.y_pos = this.max_y;
+        // Remember that top = close to zero.
+        if (this.y_pos < this.top_border) {
+            this.y_pos = this.top_border
         }
-        else if (this.y_pos < this.min_y) {
-            this.y_pos = this.min_y;
+        else if (this.y_pos > this.bottom_border) {
+            this.y_pos = this.bottom_border;
         }
 
         context.fillRect(this.x_pos, this.y_pos, this.width, this.height);
     }
 }
 
-var left_paddle = new paddle(15, canvas.height/2, true);
-var right_paddle = new paddle(canvas.width - (25 + 15), canvas.height/2, false);
+var left_paddle = new paddle(true, canvas.height/2, true);
+var right_paddle = new paddle(false, canvas.height/2, false);
 
 function draw() {
     // Draw the background.
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = "white";
-    white_width = canvas.width/4 + (Math.sin(i) * 100);
-    heigh_width = canvas.height/4 + (Math.cos(i) * 100);
-    context.fillRect(canvas.width/3, canvas.height/3, white_width, heigh_width);
-    i = i + 0.01;
-
     left_paddle.draw();
     right_paddle.draw();
 }
