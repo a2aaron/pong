c1a87bc0e9175d932f43cdadb5ac64f59976fded
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-06-24 18:33:56 -0400

    Make x and y position refer to the center of the paddles.
    This is a much nicer way of drawing and mananging coordinates. It does make the widths
    and heights kind of screwy though.
    
    A few functions have been made to abstract away drawing (one is a debug function).

--- pong.js
+++ pong.js
@@ -9,39 +9,46 @@ function paddle(is_left, y_pos, is_human) {
     // Coordinates refer to the upper left corner of the paddle.
     // IDEA: Change to exact center of paddle.
     if(is_left) {
-        this.x_pos = 20;
+        this.x_pos = 30;
     } else {
-        this.x_pos = canvas.width - (this.width + 20)
+        this.x_pos = canvas.width - (this.width/2 + 30)
     }
     this.y_pos = y_pos;
     this.is_human = is_human;
 
     // Maximum allowed distance to the edges.
     this.top_border = 10;
-    this.bottom_border = canvas.height - (this.top_border + this.height);
+    this.bottom_border = canvas.height - 10;
     this.left_border = 20;
-    this.right_border = 20;
+    this.right_border = canvas.width - 20;
 
-    this.update = function() {
+    this.update = function(ball) {
         if (is_human) {
-            this.y_pos = mousePos.y - this.height/2 // Center paddle.
+            this.y_pos = mousePos.y; // Center paddle.
         } else {
-            this.y_pos = canvas.height/2 + Math.cos(ticks/10)*canvas.height/4;
+            this.y_pos = this.y_pos + (ball.y_pos - this.y_pos)/10;
         }
 
-        // Prevent paddle from leaving screen.
-        // Remember that top = close to zero.
-        if (this.y_pos < this.top_border) {
-            this.y_pos = this.top_border
+        this.prevent_exit();
+    }
+
+    this.prevent_exit = function() {
+        if (this.y_pos < this.top_border + this.height/2) {
+            this.y_pos = this.top_border + this.height/2;
+        } else if (this.y_pos > this.bottom_border - this.height/2) {
+            this.y_pos = this.bottom_border - this.height/2;
         }
-        else if (this.y_pos > this.bottom_border) {
-            this.y_pos = this.bottom_border;
+
+        if (this.x_pos < this.left_border + this.width/2) {
+            this.x_pos = this.left_border + this.width/2;
+        } else if (this.x_pos > this.right_border - this.width/2) {
+            this.x_pos = this.right_border - this.width/2;
         }
     }
 
-
     this.draw = function() {
-        context.fillRect(this.x_pos, this.y_pos, this.width, this.height);
+        centered_rect(this);
+        cross_hairs(this);
     }
 }
 
@@ -50,34 +57,52 @@ function ball(x_pos, y_pos) {
     this.y_pos = y_pos;
     this.width = 10;
     this.height = 10;
-    this.top_border = 3;
-    this.bottom_border = canvas.height - (this.height + 3);
-    this.left_border = 3 + this.width;
-    this.right_border = 3 - this.width;
+    this.top_border = 3
+    this.bottom_border = canvas.height - 3;
+    this.left_border = 3;
+    this.right_border = canvas.width - 3;
 
     this.update = function() {
-        this.move(20*Math.cos(ticks/5),20*Math.sin(ticks/10));
+        this.y_pos = canvas.height/2 + canvas.height * Math.sin(ticks/50);
+        this.x_pos = canvas.width/2 + canvas.height * Math.cos(ticks/50); 
+        this.prevent_exit();
     }
 
     this.move = function(x, y) {
         this.x_pos += x;
         this.y_pos += y;
-        if (this.y_pos < this.top_border) {
-            this.y_pos = this.top_border;
-        } else if (this.y_pos > this.bottom_border) {
-            this.y_pos = this.bottom_border;
+    }
+
+    this.prevent_exit = function() {
+        if (this.y_pos < this.top_border + this.height/2) {
+            this.y_pos = this.top_border + this.height/2;
+        } else if (this.y_pos > this.bottom_border - this.height/2) {
+            this.y_pos = this.bottom_border - this.height/2;
         }
 
-        if (this.x_pos < this.left_border) {
-            this.x_pos = this.left_border;
-        } else if (this.x_pos < this.right_border) {
-            this.x_pos = this.right_border
+        if (this.x_pos < this.left_border + this.width/2) {
+            this.x_pos = this.left_border + this.width/2;
+        } else if (this.x_pos > this.right_border - this.width/2) {
+            this.x_pos = this.right_border - this.width/2;
         }
     }
 
     this.draw = function() {
-        context.fillRect(this.x_pos, this.y_pos, this.width, this.height);
+        centered_rect(this);
+        cross_hairs(this);
     }
+}
+
+// TODO: move to super class?
+function centered_rect(obj) {
+    context.fillRect(obj.x_pos - obj.width/2, obj.y_pos - obj.height/2,
+                     obj.width, obj.height);
+}
+
+// TODO: move to super class?
+function cross_hairs(obj) {
+    context.fillRect(0, obj.y_pos, canvas.width, 1);
+    context.fillRect(obj.x_pos, 0, 1, canvas.height);
 }
 
 var left_paddle = new paddle(true, canvas.height/2, true);
@@ -108,15 +133,16 @@ function getMousePosition(e) {
 
 var ticks = 0;
 function main() {
-    document.addEventListener("mousemove", function(e) {
-        mousePos = getMousePosition(e);
-    }, false);
+
     left_paddle.update();
-    right_paddle.update();
+    right_paddle.update(ball);
     ball.update();
     draw();
     ticks += 1;
 }
 
+document.addEventListener("mousemove", function(e) {
+    mousePos = getMousePosition(e);
+}, false);
 
-setInterval(main, 50)
+setInterval(main, 20);
